1de4eaf5956ddb0a2c2f3388e3a7f6252305aacd
Ted Tomlinson <zumbino@gmail.com>
2011-12-04 18:23:22 -0500

    initial layout of tabs

---
+++ suruga_home/CompareTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  suruga_home
+//
+//  Created by Ted Tomlinson on 12/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompareTableViewController : UIViewController
+
+@end
---
+++ suruga_home/TaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  suruga_home
+//
+//  Created by Ted Tomlinson on 12/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TaskViewController : UIViewController
+
+@end
