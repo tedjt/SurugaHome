f343d5f4da76ca7a41231435a3c92dd797a09157
Ted Tomlinson <zumbino@gmail.com>
2011-12-04 16:42:44 -0500

    Initial Commit

---
+++ suruga_home/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  suruga_home
+//
+//  Created by Ted Tomlinson on 12/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
---
+++ suruga_home/suruga_homeAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  suruga_homeAppDelegate.h
+//  suruga_home
+//
+//  Created by Ted Tomlinson on 12/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface suruga_homeAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
